1e8240466fd64539357165482a4b116494e90bf7
Raghavendra Kudva <a0875405@ti.com>
2013-01-14 21:17:17 +0530

    IPNC : DMA channel allocation fro IPNC application

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -1639,6 +1639,7 @@ static struct platform_device ti816x_edma_device = {
 static const s16 ti814x_dma_rsv_chans[][2] = {
 	/* (offset, number) */
 	{0, 2},
+	{5, 5},
 	{14, 2},
 	{26, 6},
 	{48, 4},
@@ -1649,6 +1650,7 @@ static const s16 ti814x_dma_rsv_chans[][2] = {
 static const s16 ti814x_dma_rsv_slots[][2] = {
 	/* (offset, number) */
 	{0, 2},
+	{5, 5},
 	{14, 2},
 	{26, 6},
 	{48, 4},
